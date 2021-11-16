eaca3c4554c0dedb7474545429e83622f9a2b795
AdamAbd <adamabd14113@gmail.com>
2021-11-16 15:00:08 +0700

    models

---
+++ lib/models/User.dart
@@ -0,0 +1,12 @@
+class User {
+  final String name, image;
+
+  User(this.name, this.image);
+}
+
+List<User> topTravelers = [user1, user2, user3, user4];
+
+User user1 = User('James', 'assets/images/james.png');
+User user2 = User("John", "assets/images/John.png");
+User user3 = User("Marry", "assets/images/marry.png");
+User user4 = User("Rosy", "assets/images/rosy.png");
